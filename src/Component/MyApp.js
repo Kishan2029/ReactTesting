a658d3758130541bdc8221aecaeab76d0d1d4430
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-02-27 10:57:45 +0530

    init1

---
+++ src/Component/MyApp.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Footer from "./Footer.js";
+function MyApp()
+{
+  return(
+  <div>
+      <nav>
+      <h2>Hi</h2>
+      <p>How are you</p>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </nav>
+    <main>
+      <p>This is where the main conent is...</p>
+    </main>
+    <Footer/>
+  </div>
+   )
+}
+export default MyApp;
