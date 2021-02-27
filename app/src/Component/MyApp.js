fda4b3630709544ff4f14d1b4b0c627adf5bfab5
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-02-27 15:34:06 +0530

    todoapp2 and Props

--- app/src/Component/MyApp.js
+++ app/src/Component/MyApp.js
@@ -1,19 +1,37 @@
 import React from "react";
-import MainContent from "./MainContent";
-import Header from "./Header";
-import Footer from "./Footer";
 import "../style.css"
+import ContactCard from "./ContactCard";
 function MyApp()
 {
   return(
-  <div>
-    
-        <Header/>
-        <MainContent/>
-        <Footer/>
-    
-  
-  </div>
+      <div className="contacts"> 
+            <ContactCard 
+            name="Kishan"
+            imagUrl="app\src\Image\dog1.jpg"
+            phone="12156"
+            email="kishan@gmail.com"/>
+
+            <ContactCard 
+            name="Rohan"
+            imagUrl="app\src\Image\dog2.jpg"
+            phone="51616"
+            email="rohann@gmail.com"/>
+
+            <ContactCard 
+            name="Rahul"
+            imagUrl="app\src\Image\dog3.jpg"
+            phone="161651"
+            email="rahul@gmail.com"/>
+
+            <ContactCard 
+            name="Bhavya"
+            imagUrl="app\src\Image\dog4.jpg"
+            phone="15163456"
+            email="rutvik@gmail.com"/>
+      </div>
+
+      
+      
    )
 }
 export default MyApp
