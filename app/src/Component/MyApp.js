0d758e309c2455928cf9f98c2a2339a562a8ef69
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-03-04 23:03:55 +0530

    Changing state and todo app6

--- app/src/Component/MyApp.js
+++ app/src/Component/MyApp.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
-function handleClick(){
-      console.log("I was Clicked");
-}
-function MyApp(){
-      return(
-            <div>
-                  <img onMouseOver={() => console.log("Hovered")} src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"/>
-                  <br/><br/>
-                  <button onClick={handleClick}>Click me</button>
-            </div>
-      )
+
+class MyApp extends React.Component{
+      constructor()
+      {
+            super();
+            this.state={
+                  count:0
+            }
+            this.handleClick=this.handleClick.bind(this)
+      }
+      handleClick(){
+           
+            this.setState(prevState=>{
+                  return{
+                       count: prevState.count +1 
+                  }
+                  })
+      }
+      render()
+      {
+            return(
+                  <div>
+                        <h1>{this.state.count}</h1>
+                        <button onClick={this.handleClick}>Click me</button>
+                  </div>
+            )
+      }
+     
 }
 export default MyApp;
